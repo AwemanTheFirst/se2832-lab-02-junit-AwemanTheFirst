153b3907b3ba9deb69df5fd81adacd91fcc64b3a
Ryan Weise <weiserd@msoe.edu>
2017-03-16 10:49:52 -0500

    Added initial draft of tests

--- test/CircularQueueTest.java
+++ test/CircularQueueTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
  * Brad Dennis
  * 3/9/2017
  */
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 public class CircularQueueTest {
     @Before
     public void setUp() throws Exception {
@@ -498,7 +499,7 @@ public class CircularQueueTest {
         CircularQueue<String> queue = new CircularQueue<>(10);
 
         //Act & Assert
-        Object[] objects = queue.toArray(new String[] {});
+        Object[] objects = queue.toArray(new String[queue.size()]);
     }
 
     @Test
